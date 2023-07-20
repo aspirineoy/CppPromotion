5d19038997fb5eaad5da187011c7b60852805043
aspirineoy <aspirineoy@qq.com>
2023-07-21 01:09:24 +0800

    4-6

--- CppPromotion/StackInfo.h
+++ CppPromotion/StackInfo.h
@@ -1,5 +1,45 @@
 #pragma once
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class BaseData {
+public:
+	virtual ~BaseData(){
+
+	}
+};
+
+template<typename T>
+class Data :public BaseData {
+public:
+	Data(T data):mdata(data){}
+	T ReturnData() {
+		return mdata;
+	}
+private:
+	T mdata;
+};
+
 class StackInfo
 {
+public:
+	template<class T>
+	void push(T data) {
+		datas.push_back(make_shared<Data<T>>(data));
+	}
+
+	template<class T>
+	void pop(T& data) {
+		auto index = dynamic_cast<Data<T>&>(*datas.back());
+		data = index.ReturnData;
+		datas.pop_back();
+	}
+
+private:
+	vector<BaseData*> datas;
 };
 
--- CppPromotion/main.cpp
+++ CppPromotion/main.cpp
@@ -51,8 +51,8 @@ void test2()
     stack.pop(ret);
     std::cout << "testWithStack ret " << ret << std::endl;
 }
-
 */
+
 int main() {
 	auto str1 = CustomString("test1");
 	auto str2 = CustomString("test2, test3");
